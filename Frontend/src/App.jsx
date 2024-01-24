4815a7416de7d647b319bdda25632fd2650e0f85
mubashir-mehran <mmehran056@gmail.com>
2024-01-24 15:21:31 +0500

    Implemented Toast Notification

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,20 +1,19 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./Login";
-import Signup from "./SignupScreen";
 import AddProject from "./Dashboard";
 import AllProjectsPage from "./Projects";
 import AchievedProjectsPage from "./ArchievedProjects";
 import CompeletedProjectsPage from "./CompletedProjects";
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+import SignupPage from "./SignupScreen";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route exact="true" path="/" element={<LoginPage />}></Route>
-        <Route path="/register" element={<Signup />}></Route>
+        <Route path="/register" element={<SignupPage />}></Route>
         <Route path="/dashboard" element={<AddProject />}></Route>
         <Route
           path="/dashboard/projects/all"
